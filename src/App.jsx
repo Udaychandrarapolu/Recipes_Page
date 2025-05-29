37b0cb6be4f6f01d84866748a8f5ec6fbc22a6eb
Udaychandrarapolu <uday.rapolu112@gmail.com>
2025-05-29 12:00:31 +0530

    React Router DOM usage and RecipesDisplay

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import Navbar from "./Components/Navbar/Navbar.jsx";
 import About from "./Components/About/About.jsx";
 import Contact from "./Components/Contact/Contact.jsx";
 import Services from "./Components/Services/Services.jsx";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import RecipesDisplay from "./Components/RecipesDisplay.jsx";
 
 
-const App=() => {
+const App = () => {
   return (
     <>
       <Navbar></Navbar>
       <Routes>
-        <Route path="/About" element={<About/>}></Route>
-        <Route path="/Contact" element={<Contact/>}></Route>
-        <Route path="/Services" element={<Services/>}/>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+        <Route path="/Services" element={<Services />} />
       </Routes>
       <RecipesDisplay></RecipesDisplay>
     </>
